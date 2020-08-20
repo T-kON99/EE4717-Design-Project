8c31689d8a6f796d27bcd7ee908c824947365311
Kenneth Anderson <eucliwood98@gmail.com>
2020-08-20 14:01:38 +0800

    Given VM Server is using PHP5. public const is only for PHP7 and above.

--- components/theme.php
+++ components/theme.php
@@ -3,15 +3,15 @@
         Set out dynamic variable for us to use.
     */
     class ThemeData {
-        public const navbarColor_nonactive = "rgb(52, 107, 174)";
-        public const navbarColor_hover = "rgb(52, 107, 174)";
-        public const navbarColor_click = "rgb(52, 107, 174)";
-        public const navbarColor_active = "rgb(52, 107, 174)";
+        const navbarColor_nonactive = "rgb(52, 107, 174)";
+        const navbarColor_hover = "rgb(52, 107, 174)";
+        const navbarColor_click = "rgb(52, 107, 174)";
+        const navbarColor_active = "rgb(52, 107, 174)";
         
-        public const navbarTextColor_nonactive = "white";
-        public const navbarTextColor_hover = "rgb(10, 173, 203)";
-        public const navbarTextColor_click = "white";
-        public const navbarTextColor_active = "rgb(10, 173, 203)";
+        const navbarTextColor_nonactive = "white";
+        const navbarTextColor_hover = "rgb(10, 173, 203)";
+        const navbarTextColor_click = "white";
+        const navbarTextColor_active = "rgb(10, 173, 203)";
 
         public static $urls = array(
             'Home' => 'index.php',
