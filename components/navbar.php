4aa607cdb15560a12155c0bd924319d06f17636f
Kenneth Anderson <eucliwood98@gmail.com>
2020-08-25 03:34:34 +0800

    removed unused code. Added id.

--- components/navbar.php
+++ components/navbar.php
@@ -1,10 +1,9 @@
 <?php namespace Main\Navbar;?>
 <?php set_include_path(__DIR__.'/') ?>
 <?php include_once('theme.php');?>
-<div class="navbar-wrapper">
+<div class="navbar-wrapper" id="navbar-wrapper">
     <ul class="navbar" id="navbar">
         <?php
-            use Main;
             foreach(\Main\ThemeData::$urls as $name => $link) {
                 print '<li class="nav-item ' . (($currentPage === $name) ? 'active"' : '"') . '>' . '<a ' . 'href="' . $link . '">' . $name . '</a></li>';
             }
