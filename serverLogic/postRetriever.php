687a472d848b7c9f1371a0c39edf49ef8b26c507
cocopeanutz <edobiasa@gmail.com>
2020-09-01 23:46:52 +0800

    fix/sqlInjectionVuln

--- serverLogic/postRetriever.php
+++ serverLogic/postRetriever.php
@@ -7,20 +7,36 @@
     // $dateSql = date('Y-m-d H:i:s');
 
     $conn = connectDatabase();
-    $queryBooked = "SELECT doctor, username, time FROM appointmentTable
-        WHERE doctor = '$doctorSql' AND username = '$usernameSql'
-        AND time = '$slotTimeString'";
-    $queryDelete = "DELETE FROM appointmentTable
-        WHERE doctor = '$doctorSql' AND username = '$usernameSql'
-        AND time = '$slotTimeString'";
-    $queryInsert = "INSERT INTO appointmentTable (username, doctor, time)
-    VALUES ('". $usernameSql ."','". $doctorSql ."'
-        ,'". $slotTimeString ."')";
 
-    $queryAns = mysqli_query($conn, $queryBooked);
+    $queryBooked = "SELECT doctor, username, time FROM appointmentTable
+        WHERE doctor = ? AND username = ?
+        AND time = ?";
+    $queryDelete = "DELETE FROM appointmentTable
+        WHERE doctor = ? AND username = ?
+        AND time = ?";
+    $queryInsert = "INSERT INTO appointmentTable (doctor, username, time)
+    VALUES (?,?,?)";
+
+    // prepare and bind
+    $prepareBooked = $conn->prepare($queryBooked);
+    $prepareDelete = $conn->prepare($queryDelete);
+    $prepareInsert = $conn->prepare($queryInsert);
+
+    $prepareBooked->bind_param("sss", $doctorSql, $usernameSql, $slotTimeString);
+    $prepareDelete->bind_param("sss", $doctorSql, $usernameSql, $slotTimeString);
+    $prepareInsert->bind_param("sss", $doctorSql, $usernameSql, $slotTimeString);
+
+
+
+    $prepareBooked->execute();
+    $queryAns = $prepareBooked->get_result();
     if($queryAns->num_rows){
-        mysqli_query($conn, $queryDelete);
+        $prepareDelete->execute();
     }else{
-        mysqli_query($conn, $queryInsert);
+        $prepareInsert->execute();
     }
+
+    $prepareBooked->close();
+    $prepareDelete->close();
+    $prepareInsert->close();
 ?>
