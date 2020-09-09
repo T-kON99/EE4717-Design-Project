975bce16f495e585cdd68b9defbcd1ed6d1ff4ad
Kenneth Anderson <eucliwood98@gmail.com>
2020-09-10 01:21:15 +0800

    WIP about page.

--- pages/about.php
+++ pages/about.php
@@ -12,21 +12,54 @@
     <?php include('../components/header.php'); ?>
     <?php include('../components/navbar.php'); ?>
     <div class="root">
-        This is the <?php echo $currentPage;?> page. Fill this with something....
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-        test
+        <?php include('../php/info.php'); ?>
+        <div class="content">
+            An accessible, high performing clinic that responds fast to your daily needs! We've been around since 1989.
+        </div>
+        <section class="info-banner">
+            <div class="banner">
+                <div class="container">
+                    <div class="main-banner">
+                        <div class="banner-item">
+                            <div class="box-item">
+                                <div class="box-counter"><?php echo $n_patients; ?></div>
+                                <div class="box-title">
+                                    Patients
+                                    <br>
+                                    Registered
+                                </div>
+                            </div>
+                        </div>
+                        <div class="banner-item">
+                            <div class="box-item">
+                                <div class="box-counter"><?php echo $n_patients; ?></div>
+                                <div class="box-title">
+                                    Doctors 
+                                    <br>
+                                    Registered
+                                </div>
+                            </div>
+                        </div>
+                        <div class="banner-item">
+                            <div class="box-item">
+                                <div class="box-counter"><?php echo $n_patients; ?></div>
+                                <div class="box-title">
+                                    Patients
+                                    <br>
+                                    Treated
+                                </div>
+                            </div>
+                        </div>
+                        <div class="banner-item">
+                            <div class="box-item">
+                                <div class="box-counter"><?php echo $n_patients; ?></div>
+                                <div class="box-title">Rating</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
     <?php include('../components/footer.php'); ?>
 </body>
