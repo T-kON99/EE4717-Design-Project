f30f9775e0095397b6208c25034bf817aa92ad38
Kenneth Anderson <eucliwood98@gmail.com>
2020-10-07 17:34:28 +0800

    git wrong stash pop

--- js/classes/FormValidator.js
+++ js/classes/FormValidator.js
@@ -1,58 +1,53 @@
 import { Validator } from './Validator.js';
 
-window.onload = () => {
-    let formHTML = document.getElementById('formcontact');
-    let form = {
-        'subject': formHTML.getElementById('subject'),
-        'body': formHTML.getElementById('email'),
-    };
-    let validator = new Validator(form, {
-        'subject': (value) => String(value).length > 0,
-        'body': (value) => String(value).length > 0
-    });
-    // for(const field in form) {
-    //     form[field].onchange = (event) => {
-    //         let error = document.getElementById(`${field}-error`);
-    //         let error_message = '';
-    //         if(!validator[`validate_${field}`](event.target.value)) {
-    //             error.classList.toggle('blink');
-    //             setTimeout(() => {
-    //                 error.classList.toggle('blink');
-    //             }, 2000);
-    //             switch(field) {
-    //                 case 'name':
-    //                     error_message = 'Name must only contain alphabetic and space characters!';
-    //                     break;
-    //                 case 'email':
-    //                     error_message = 'Email must follow given format!';
-    //                     break;
-    //                 case 'startdate':
-    //                     error_message = 'Start date can\'t be today or past dates!';
-    //                     break;
-    //                 case 'experience':
-    //                     error_message = 'Experience can\'t be empty!';
-    //                     break;
-    //             }
-    //         } else {
-    //             // Remove if it's validated.
-    //             if(error.classList.contains('blink')) {
-    //                 error.classList.remove('blink');
-    //             }
-    //         }
-    //         error.innerHTML = error_message;        
-    //     };
-    // }
-    // formHTML.onsubmit = (event) => {
-    //     if(!validator.validateAll()) {
-    //         event.preventDefault();
-    //         alert('Please fix the error!');
-    //         for(const field in form) {
-    //             var event = new Event('change');
-    //             form[field].dispatchEvent(event);
-    //         }
-    //         return false;
-    //     }
-    //     return true;
-    // }
-    console.log(validator.validateAll());
+export class FormValidator {
+    constructor(formElement, formValidator = {}, errorMessage = {}) {
+        this.form = formElement;
+        this.inputs = formElement.querySelectorAll('input,textarea');
+        this.formChildren = {};
+        this.errors = errorMessage;
+        for(const input of this.inputs) {
+            if (input?.type && input?.type !== 'submit' && input?.type !== 'reset') {
+                this.formChildren[input.name] = input;
+            }
+        }
+        this.validator = new Validator(this.formChildren, formValidator);
+    }
+
+    setupListener({showErrors = false}) {
+        let form = this.formChildren;
+        for(const field in form) {
+            form[field].onchange = (event) => {
+                let error_message = '';
+                if (showErrors) {
+                    let error = document.getElementById(`${field}-error`);
+                    if(!this.validator[`validate_${field}`](event.target.value)) {
+                        error.classList.toggle('blink');
+                        setTimeout(() => {
+                            error.classList.toggle('blink');
+                        }, 2000);
+                        error_message = this.errors[field];
+                    } else {
+                        // Remove if it's validated.
+                        if(error.classList.contains('blink')) {
+                            error.classList.remove('blink');
+                        }
+                    }
+                    error.innerHTML = error_message;       
+                } 
+            };
+        }
+        this.form.onsubmit = (event) => {
+            if(!this.validator.validateAll()) {
+                event.preventDefault();
+                alert('Please fix the error!');
+                for(const field in form) {
+                    var event = new Event('change');
+                    form[field].dispatchEvent(event);
+                }
+                return false;
+            }
+            return true;
+        }
+    }
 }
