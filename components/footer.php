d1a7084b53173ab64d7038f06777383b366d7c9e
Kenneth Anderson <eucliwood98@gmail.com>
2020-10-21 06:56:36 +0800

    more or less finished product.

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,8 @@
 <footer>
-        <small><i>Copyright &copy; 2020 Kenneth Anderson | Edo Jayakusuma</i></small>
-        <small><i><a href="mailto:kenneth@anderson.com" target="_blank">kenneth@anderson.com</a></i></small>
-        <small><i><a href="mailto:edo@jayakusuma.com" target="_blank">edo@jayakusuma.com</a></i></small>
+        <img src="../images/ntu.png" alt="ntu-logo">
+        <div class="footer-text">
+                <small><i>Copyright &copy; 2020 Kenneth Anderson | Edo Jayakusuma</i></small>
+                <small><i><a href="mailto:kenneth@anderson.com" target="_blank">kenneth@anderson.com</a></i></small>
+                <small><i><a href="mailto:edo@jayakusuma.com" target="_blank">edo@jayakusuma.com</a></i></small>
+        </div>
 </footer>
