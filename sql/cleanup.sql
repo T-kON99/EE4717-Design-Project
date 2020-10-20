f501fabfcb855e61f4d6bbcbe3f228ee8ffffa64
Kenneth Anderson <eucliwood98@gmail.com>
2020-10-20 23:48:18 +0800

    Fix small sql insertion issue.

--- sql/cleanup.sql
+++ sql/cleanup.sql
@@ -1,3 +1,3 @@
 -- This file is ignored on all scripts.
 -- Use this ONLY when you know what you're doing.
-DROP TABLE doctors_availability, doctors_messages, doctors, users, categories, appointments;
+DROP TABLE appointments, doctors_availability, doctors_messages, doctors, users, categories;
