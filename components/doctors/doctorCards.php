d9d6ce152ce4eba6b1ec4ec44b71dfab6bb43ff6
Kenneth Anderson <eucliwood98@gmail.com>
2020-10-21 06:56:09 +0800

    updated doctors page. Now shows introduction.

--- components/doctors/doctorCards.php
+++ components/doctors/doctorCards.php
@@ -2,7 +2,7 @@
 <?php
     require_once('../php/config.php');
     @session_start();
-    $sql_query = 'SELECT doctors.id, doctors.name, doctors.rating, doctors.address, doctors.image_link, categories.name AS category FROM doctors LEFT JOIN categories ON categories.id=doctors.category_id ORDER BY categories.name';
+    $sql_query = 'SELECT doctors_messages.introduction AS introduction, doctors.id, doctors.name, doctors.rating, doctors.address, doctors.image_link, categories.name AS category, categories.icon_link AS icon_link FROM doctors LEFT JOIN categories ON categories.id=doctors.category_id LEFT JOIN doctors_messages ON doctors.id=doctors_messages.doctors_id ORDER BY categories.name';
     $db = connect_db();
     $result = $db->query($sql_query);
     if(!$result) {
@@ -15,16 +15,17 @@
     $image_link = $row["image_link"];
     $category = $row["category"];
     $doctor_id = $row["id"];
+    $intro = $row["introduction"];
 ?>
 <form method="GET" action="../components/<?php echo $fname; ?>/doctorDetails.php" class="<?php echo $_SESSION["type"] === "doctor" ? "not-clickable" : "" ?> card-row-xl card-doctor hidden <?php echo str_replace(' ', '_', $category); ?>" id="<?php echo $name.'-card-'.$doctor_id; ?>">
     <div class="card-col-xl icon-xl">
         <div class="card-icon-xl"><img src="<?php echo $image_link; ?>" alt="<?php echo $name.'-icon'; ?>"></div>
     </div>
     <div class="card-col-xl description">
-        <div class="card-title-xl"><?php echo $name; ?></div>
+        <div class="card-title-xl"><?php echo $name; ?> | <?php echo $category; ?></div>
         <hr class="card-separator-xl">
         <div class="card-subtitle-xl">
-            <?php echo $category; ?>
+            <?php echo $intro; ?>
         </div>
     </div>
     <input type="hidden" name="doctor_id" value="<?php echo $doctor_id ?>">
